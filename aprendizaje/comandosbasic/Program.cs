2828aa460e742e763fab596fd3d4a3bb7978ea98
Torraskes <78329836+Torraskes@users.noreply.github.com>
2025-05-01 18:27:19 -0600

    feat: peticion de nombre y apellido

---
+++ aprendizaje/comandosbasic/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace comandosbasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nombre;
+            string apellido;
+            Console.Write("Escribe tu nombre:");
+            nombre = Console.ReadLine();
+            Console.Write("Escribe tu apellido:");
+            apellido=Console.ReadLine();
+            Console.WriteLine($"Hola,{nombre} {apellido}");
+            
+        }
+    }
+}
